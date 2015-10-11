8a6d485adbc7e17ec50109a5f318c272289def5d
Javier Fernandes <javier.fernandes@gmail.com>
2015-10-11 16:22:01 -0300

    enhancing version changer script

--- changeVersions.sh
+++ changeVersions.sh
@@ -2,34 +2,74 @@
 
 NEW_VERSION=$1
 
-echo "Updating pom files..."
+echo "Updating to version $NEW_VERSION:"
+
+echo -e "\t - POM"
 for i in `find . -name "pom.xml" -not -path "./.metadata/*" -not -path "*/target/*" -not -path "*/META-INF/maven/*"`; do
     sed -e "s#\(.*\)<version>[0-9][\.0-9]*-SNAPSHOT</version>\(.*\)#\1<version>$NEW_VERSION-SNAPSHOT</version>\2#g" $i > $i.tmp
     rm $i
     mv $i.tmp $i
 done
 
-echo "Updating bundle versions in MANIFEST.MF..."
-for i in `find . -name "MANIFEST.MF" | grep uqbar | grep -v "/target"`;	do
+echo -e "\t - MANIFEST VERSIONS"
+for i in `find . -name "MANIFEST.MF" | grep wollok | grep -v "/target"`;	do
     sed -e "s#Bundle-Version: \(.*\)#Bundle-Version: $NEW_VERSION.qualifier#g" $i > $i.tmp
     rm $i
     mv $i.tmp $i
 done
 
-echo "Updating dependencies in MANIFEST.MF ..."
+echo -e "\t - MANIFESTS DEPENDENCIES"
 for i in `find . -name "MANIFEST.MF" | grep uqbar | grep -v "/target"`; do 
     grep "bundle-version=\"" $i | grep uqbar > /dev/null
     if [ $? -eq 0 ] ; then
- 	echo "Modifying $i..."
+ 	# echo "Modifying $i..."
 	sed -e "s#\(.*\)org\.uqbar\(.*\)bundle-version=\"[0-9][\.0-9]*\"\(.*\)#\1org\.uqbar\2bundle-version=\"$NEW_VERSION\"\3#g" $i > $i.tmp
     	rm $i
     	mv $i.tmp $i
     fi
 done
 
-echo "Updating features ..."
+echo -e "\t - FEATURES"
 for i in `find . -name "feature.xml" | grep uqbar | grep -v "/target"`; do
     sed -e "s#\(.*\)version=\"[0-9][\.0-9]*\.qualifier\"\(.*\)#\1version=\"$NEW_VERSION.qualifier\"\2#g" $i > $i.tmp
     rm $i
     mv $i.tmp $i
+
+    sed -e "s#\(.*\)plugin=\"org\.uqbar\.project\.wollok\" version=\"[0-9][\.0-9]*\"\(.*\)#\1plugin=\"org.uqbar.project.wollok\" version=\"$NEW_VERSION\"\2#g" $i > $i.tmp
+    rm $i
+    mv $i.tmp $i
+
 done
+
+echo -e "\t - PRODUCT"
+for i in `find . -name "*.product" -type f | grep uqbar | grep -v "/target"`; do
+    sed -e "s#[0-9]\.[0-9]\.[0-9]\.qualifier#$NEW_VERSION.qualifier#g" $i > $i.tmp
+    rm $i
+    mv $i.tmp $i
+
+    sed -e "s#Version\ [0-9]\.[0-9]\.[0-9]#Version\ $NEW_VERSION#g" $i > $i.tmp
+    rm $i
+    mv $i.tmp $i
+done
+
+
+echo -e "\t - CATEGORY"
+for i in `find . -name "category.xml" -type f | grep uqbar | grep -v "/target"`; do
+    sed -e "s#[0-9]\.[0-9]\.[0-9]\.qualifier#$NEW_VERSION.qualifier#g" $i > $i.tmp
+    rm $i
+    mv $i.tmp $i
+done
+
+echo -e "\t - CODE"
+i=`find . -name "Wollok.xtend" -not -path "*/target/*" `
+sed -e "s#[0-9]\.[0-9]\.[0-9]#$NEW_VERSION#g" $i > $i.tmp
+rm $i
+mv $i.tmp $i
+
+echo -e "\t - ABOUT TEXT"
+i="org.uqbar.project.wollok.product/plugin.xml"
+sed -e "s#Version [0-9]\.[0-9]\.[0-9]#Version $NEW_VERSION#g" $i > $i.tmp
+rm $i
+mv $i.tmp $i
+
+echo "Done !"
--- org.uqbar.project.wollok.launch/src/org/uqbar/project/wollok/launch/WollokChecker.xtend
+++ org.uqbar.project.wollok.launch/src/org/uqbar/project/wollok/launch/WollokChecker.xtend
@@ -42,7 +42,7 @@ class WollokChecker {
 			log.debug(" args: " + args.toList)
 			
 			if (args.contains("--version")) {
-				println("Wollok v1.1.0") // TODO: compute real version number 
+				println("Wollok v" + Wollok.VERSION) 
 				System.exit(0)
 			}
 			
