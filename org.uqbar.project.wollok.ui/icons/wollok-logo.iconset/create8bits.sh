f9a5b92ad8db67716ad12d20ff75fa10877f8ef3
Pablo Tesone <tesonep@gmail.com>
2017-07-10 18:08:17 +0200

    Fixing the icon in windows

--- org.uqbar.project.wollok.ui/icons/wollok-logo.iconset/create8bits.sh
+++ org.uqbar.project.wollok.ui/icons/wollok-logo.iconset/create8bits.sh
@@ -3,3 +3,6 @@
 convert icon_16x16.png -colors 256 icon_16x16-8bits.png
 convert icon_32x32.png -colors 256 icon_32x32-8bits.png
 convert icon_48x48.png -colors 256 icon_48x48-8bits.png
+convert icon_64x64.png -colors 256 icon_64x64-8bits.png
+convert icon_128x128.png -colors 256 icon_128x128-8bits.png
+convert icon_256x256.png -colors 256 icon_256x256-8bits.png
