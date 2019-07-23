0f34c3be6b928c1b15a8305a880e7bcc6e76c1c1
Fernando Dodino <fernando.dodino@gmail.com>
2019-07-22 22:48:40 -0300

    Testing correct folder again

--- scripts/generateWollokDoc.sh
+++ scripts/generateWollokDoc.sh
@@ -1,2 +1,6 @@
 echo "Generating WollokDoc..."
+
+java -cp org.uqbar.project.wollok.launch/target/org.uqbar.project.wollok.launch-1.7.7-SNAPSHOT.jar org.uqbar.project.wollok.wollokDoc.WollokDocParser
+
 echo "...done"
+exit 0
