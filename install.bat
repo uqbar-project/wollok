177abf2d402a0e335640fdeb798a8b1aec5ae6bf
Fernando Dodino <fernando.dodino@gmail.com>
2019-11-01 23:47:18 -0300

    Fix appveyor script & travis script

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 git submodule update --init --recursive
-for %%f in (./wollok-language/src/wollok/*.wlk) do mklink %%f "../../org.uqbar.project.wollok.lib/src/wollok/%%f"
+for %%f in (./wollok-language/src/wollok/*.wlk) do mklink %%f "./org.uqbar.project.wollok.lib/src/wollok/%%f"
