00ca61fd2e5699b903c12369e74135977dd9f85f
Fernando Dodino <fernando.dodino@gmail.com>
2019-11-27 13:04:59 -0300

    Trying to fix surefire for Build travis

--- wollokInstallTravis.sh
+++ wollokInstallTravis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git submodule update --init --recursive
 cd org.uqbar.project.wollok.lib/src/wollok
-ln ../../../wollok-language/src/wollok/*.wlk ./
+cp ../../../wollok-language/src/wollok/*.wlk ./
