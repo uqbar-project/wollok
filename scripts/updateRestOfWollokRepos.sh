f4037ce1771c0d34298947f10f8e317fb6f93942
Fernando Dodino <fernando.dodino@gmail.com>
2021-06-07 23:08:26 -0300

    Using wollok-cli & wollok-site - test

--- scripts/updateRestOfWollokRepos.sh
+++ scripts/updateRestOfWollokRepos.sh
@@ -1,5 +1,3 @@
-# openssl aes-256-cbc -k "$travis_key_password" -d -md sha256 -a -in wolloksite_key.enc -out /tmp/wolloksite_key
-# chmod 600 /tmp/wolloksite_key
 eval `ssh-agent -s`
 
 echo "Cloning wollok-cli repo..."
@@ -13,55 +11,58 @@ echo "Copying Wollok library files into libs folder"
 mkdir -p ./libs
 cp ../org.uqbar.project.wollok.lib/src/wollok/*.wlk ./libs
 
-# echo "****************************************************************************"
-# echo "Cloning Wollok Site repo"
-# git clone git@wollok_site:uqbar-project/wollok-site.git
-# rm  ./wollok-site/documentacion/wollokdoc/*.html
-# rm  ./wollok-site/documentacion/wollokDoc.md
-# mkdir -p ./wollok-site/documentacion/wollokdoc
+echo "****************************************************************************"
+echo "Cloning Wollok Site repo"
+git clone git@wollok_site:uqbar-project/wollok-site.git
+rm  ./wollok-site/documentacion/wollokdoc/*.html
+rm  ./wollok-site/documentacion/wollokDoc.md
+mkdir -p ./wollok-site/documentacion/wollokdoc
 
-# echo "Updating spanish WollokDoc files"
-# java -cp "./wollok-cli/jars/*" org.uqbar.project.wollok.wollokDoc.WollokDocParser "./libs" -folder "./wollok-site/documentacion/wollokdoc" -locale "es"
+echo "Updating spanish WollokDoc files"
+java -cp "./wollok-cli/jars/*" org.uqbar.project.wollok.wollokDoc.WollokDocParser "./libs" -folder "./wollok-site/documentacion/wollokdoc" -locale "es"
 
-# echo
-# echo "List of files generated"
-# ls   ./wollok-site/documentacion/wollokdoc
+echo
+echo "List of files generated"
+ls   ./wollok-site/documentacion/wollokdoc
 
 # generamos la documentación en inglés
 # no copiamos el wollokDoc.md, si hay nuevos archivos o se elimina alguno hay que 
 # ajustarlo manualmente
-# echo "Updating english WollokDoc files"
-# java -cp "./wollok-cli/jars/*" org.uqbar.project.wollok.wollokDoc.WollokDocParser "./libs" -folder "./wollok-site/en/documentation/wollokdoc" -locale "en"
+echo "Updating english WollokDoc files"
+java -cp "./wollok-cli/jars/*" org.uqbar.project.wollok.wollokDoc.WollokDocParser "./libs" -folder "./wollok-site/en/documentation/wollokdoc" -locale "en"
 
-# echo
-# echo "List of files generated"
-# ls   ./wollok-site/en/documentation/wollokdoc
-# echo "****************************************************************************"
+echo
+echo "List of files generated"
+ls   ./wollok-site/en/documentation/wollokdoc
+echo "****************************************************************************"
 
 echo "List of wollok-cli files updated"
 cd wollok-cli
-echo $TRAVIS_JOB_ID > wollok-cli.lastUpdated
+echo $GITHUB_JOB > wollok-cli.lastUpdated
 git status
 echo ""
 echo "============================================================================"
 echo "Pushing wollok-cli files"
+BRANCH_ID=next-version-$GITHUB_JOB
+git checkout -b $BRANCH_ID
 git add .
 git commit -m "Updating Wollok JARs library from Github Action => $GITHUB_JOB"
-git push -v origin master
+git push --set-upstream -v origin $BRANCH_ID
 echo "****************************************************************************"
 
-# alias ssh='ssh -F <(cat .ssh/config /tmp/wolloksite_key)'
-# cd ../wollok-site
-# echo $TRAVIS_JOB_ID > wollok-site.lastUpdated
-# echo "List of wollok-site files updated"
-# git status
-# echo ""
-# echo "============================================================================"
-# echo "Pushing wollok-site files"
-# git add .
-# git commit -m "Updating Wollokdoc files from Travis Job $TRAVIS_JOB_ID"
-# git push -v origin master
-# echo "****************************************************************************"
+alias ssh='ssh -F <(cat .ssh/config ~/.ssh/wolloksite_key)'
+cd ../wollok-site
+echo $GITHUB_JOB > wollok-site.lastUpdated
+echo "List of wollok-site files updated"
+git status
+echo ""
+echo "============================================================================"
+echo "Pushing wollok-site files"
+git checkout -b $BRANCH_ID
+git add .
+git commit -m "Updating Wollokdoc files from Github Action $GITHUB_JOB"
+git push --set-upstream -v origin $BRANCH_ID
+echo "****************************************************************************"
 
 echo "...done"
 exit 0
