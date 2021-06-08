e9bc30f7a917f5c6e716deb572e9c3b920e9396e
Fernando Dodino <fernando.dodino@gmail.com>
2021-06-08 19:28:18 -0300

    Using git rquest-pull

--- scripts/updateRestOfWollokRepos.sh
+++ scripts/updateRestOfWollokRepos.sh
@@ -32,7 +32,7 @@ git checkout -b $BRANCH_ID
 git add .
 git commit -m "Updating Wollok JARs library from Github Action => $GITHUB_RUN_ID"
 git push --set-upstream -v origin $BRANCH_ID
-gh pr create --base master --title "New Wollok Version" --body "Updating Wollok Version - Github Action $GITHUB_RUN_ID"
+git request-pull "Updating Wollok Version - Github Action $GITHUB_RUN_ID" origin/master $BRANCH_ID
 cd ..
 
 # **************************************************************************************
@@ -80,7 +80,7 @@ git checkout -b $BRANCH_ID
 git add .
 git commit -m "Updating Wollokdoc files from Github Action $GITHUB_RUN_ID"
 git push --set-upstream -v origin $BRANCH_ID
-gh pr create --base master --title "New Wollok Version" --body "Updating Wollok Version - Github Action $GITHUB_RUN_ID"
+git request-pull "Updating Wollok Version - Github Action $GITHUB_RUN_ID" origin/master $BRANCH_ID
 cd ..
 echo "****************************************************************************"
 
